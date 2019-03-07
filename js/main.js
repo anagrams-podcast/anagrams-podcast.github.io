bbdb0c8477b0f39cf2488d5c749c128713c449ed
rensramos <rensramos17@gmail.com>
2019-03-07 21:06:34 +0800

    Remove carousel fade effect

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     var swiper = new Swiper('.swiper-container', {
         noSwiping: false,
         loop: true,
-        effect: 'fade',
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
